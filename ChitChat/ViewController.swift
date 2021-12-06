e7dd7c5b5b8baeffb67fd60aba7990d348bc2010
wing <nguyenbuivn11@gmail.com>
2021-12-05 16:59:11 -0800

    Initial Commit

---
+++ ChitChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChitChat
+//
+//  Created by wingswift on 2021-12-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
